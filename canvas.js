6623059f4462c0a236b29b638ef43b83cc9f4971
rupalikarande <34995222+rupalikarande@users.noreply.github.com>
2018-01-18 11:33:26 +0530

    canvas.js

--- canvas.js
+++ canvas.js
@@ -27,4 +27,14 @@ var mouse={
    
    init();
  });
- 
+ // utility functions
+function randomIntFromRange(min,max)
+{
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+function randomcolor(colors)
+{
+  return.colors(Math.floor(Math.random()*colors.length);
+}
+  //objects
+ function object 
